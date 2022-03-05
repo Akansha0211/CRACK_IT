1cf45fcf277595db639ec26570a5f2116bc7eb96
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-03-05 10:57:46 +0530

    Time: 3 ms (92.45%), Space: 44.7 MB (49.81%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,22 +1,20 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        comSumHelper(0, candidates, target, ans, new ArrayList<>());
+        comSumHelper(0, candidates,target,ans, new ArrayList<>());
         return ans;
     }
-    public static void comSumHelper(int ind, int[]candidates, int target, List<List<Integer>>ans, List<Integer> list){
-        if(ind == candidates.length && target == 0){
-            ans.add(new ArrayList<>(list));
+    public static void comSumHelper(int ind, int[]candidates, int target, List<List<Integer>>ans, List<Integer> ds){
+        if(target == 0){
+            ans.add(new ArrayList<>(ds));
+            return;
         }
-        if(ind == candidates.length)return;
-        
-        if(ind >= candidates.length)return;
-        
+        if(ind>=candidates.length)return;
         if(candidates[ind]<=target){
-            list.add(candidates[ind]);
-            comSumHelper(ind, candidates, target-candidates[ind], ans, list);
-            list.remove(list.size()-1);
+            ds.add(candidates[ind]);
+            comSumHelper(ind, candidates, target - candidates[ind], ans, ds);
+            ds.remove(ds.size()-1);
         }
-        comSumHelper(ind+1, candidates, target, ans, list);
+        comSumHelper(ind+1, candidates, target, ans, ds);
     }
 }
