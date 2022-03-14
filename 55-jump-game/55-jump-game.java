8a95c36d68ad81ceee471bf48269c18ee1db2101
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-03-14 16:27:37 +0530

    Time: 632 ms (10.80%), Space: 70.2 MB (5.80%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,18 +1,24 @@
 class Solution {
     public boolean canJump(int[] nums) {
-       return helper(nums, 0, new Boolean[nums.length + 1]);
+      // Map<Integer,Boolean> map = new HashMap<>();
+        Boolean[] dp = new Boolean[nums.length+1];
+      return canJumpHelper(nums, 0, dp);
     }
-    public boolean helper(int[] nums, int i, Boolean[] dp) {
+    public boolean canJumpHelper(int[]nums, int i, Boolean[] dp){
         if(i >= nums.length - 1) return true;
-        if(dp[i] != null) return dp[i];
+        // if(map.containsKey(i)) return map.get(i);
+        if(dp[i] != null)return dp[i];
         int steps = nums[i];
         for(int j = i + 1; j <= i + steps; j++) {
-            if(helper(nums, j, dp) == true) {
+            if(canJumpHelper(nums, j, dp) == true) {
                 dp[i] = true;
+                // map.put(i, true);
                 return true;
             }
         }
         dp[i] = false;
+        // map.put(i, false);
         return false;
     }
 }
+
