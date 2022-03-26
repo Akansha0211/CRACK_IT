1e718556ebe73bb70723308eb57c34a3665ec2ec
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-03-26 10:53:58 +0530

    Time: 21 ms (44.05%), Space: 46.8 MB (23.13%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,22 +1,27 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        HashMap<String, Integer> map = new HashMap<>();
-        return lcsHelper(text1, text2, 0,0, map);
+        // HashMap<String, Integer> map = new HashMap<>();
+        int [][] dp = new int[text1.length()+1][text2.length()+1];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return lcsHelper(text1, text2, 0,0, dp);
     }
-    public int lcsHelper(String s1, String s2, int i , int j, HashMap<String,Integer> map){
+    public int lcsHelper(String s1, String s2, int i , int j, int[][] dp){
         if(i>=s1.length() || j>=s2.length())return 0;
-        String key = i + "#" + j;
-        if(map.containsKey(key))return map.get(key);
+        if(dp[i][j] !=-1)return dp[i][j];
         if(s1.charAt(i) == s2.charAt(j)){
-            map.put(key, 1+ lcsHelper(s1, s2, i+1, j+1, map));
-            return map.get(key);
+            // map.put(key, 1+ lcsHelper(s1, s2, i+1, j+1, map));
+            return dp[i][j] = 1+ lcsHelper(s1, s2, i+1, j+1, dp);
+            // return map.get(key);
         }
         else{
-            int choice1 = lcsHelper(s1, s2, i, j+1, map);
-            int choice2 = lcsHelper(s1, s2, i+1, j, map);
-            map.put(key, Math.max(choice1, choice2));
+            int choice1 = lcsHelper(s1, s2, i, j+1, dp);
+            int choice2 = lcsHelper(s1, s2, i+1, j, dp);
+            // map.put(key, Math.max(choice1, choice2));
             // return Math.max(choice1, choice2);
-            return map.get(key);
+            // return map.get(key);
+            return dp[i][j]  =  Math.max(choice1, choice2);
             
         }
     }
