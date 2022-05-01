869a394d722c24ff20e0c82153c1ab4cb3ca9b70
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-05-01 12:14:50 +0530

    Time: 3 ms (41.40%), Space: 42.2 MB (45.95%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,37 +1,32 @@
 class Solution {
-//     public boolean backspaceCompare(String s, String t) {
-//         Stack<Character> st_s = new Stack<>();
-//         Stack<Character> st_t = new Stack<>();
+    public boolean backspaceCompare(String s, String t) {
         
-//         for(int i = 0; i<s.length(); i++){
-//             if(s.charAt(i) == '#' && !st_s.isEmpty())st_s.pop();
-//             else{
-//                 st_s.push(s.charAt(i));
-//             }
-//         }
-//         for(int i = 0; i<t.length(); i++){
-//             if(t.charAt(i) == '#' && !st_t.isEmpty())st_t.pop();
-//             else{
-//                 st_t.push(t.charAt(i));
-//             }
-//         }
+        Stack<Character> st_s = new Stack<>();
+        Stack<Character> st_t = new Stack<>();
         
-//         System.out.println(String.valueOf(st_s));
-//         System.out.println(String.valueOf(st_t));
-//         return (String.valueOf(st_s).equals(String.valueOf(st_t)));
-//     }
-    public boolean backspaceCompare(String S, String T) {
-        return build(S).equals(build(T));
-    }
-
-    public String build(String S) {
-        Stack<Character> ans = new Stack();
-        for (char c: S.toCharArray()) {
-            if (c != '#')
-                ans.push(c);
-            else if (!ans.empty())
-                ans.pop();
+        for(int i = 0; i<s.length(); i++){
+            // if(s.charAt(i) == '#' && !st_s.isEmpty()){
+            //     st_s.pop();
+            // }else{
+            //     st_s.push(s.charAt(i));
+            // }
+            if(s.charAt(i) != '#'){
+                st_s.push(s.charAt(i));
+            }
+            else if(!st_s.isEmpty()){
+                st_s.pop();
+            }
         }
-        return String.valueOf(ans);
+        
+        for(int i = 0; i<t.length(); i++){
+            if(t.charAt(i) != '#'){
+                st_t.push(t.charAt(i));
+            }
+            else if(!st_t.isEmpty()){
+                st_t.pop();
+            }
+        }
+        
+        return String.valueOf(st_s).equals(String.valueOf(st_t));
     }
 }
