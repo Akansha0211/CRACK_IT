a7b2909c300df15ca596f19b43575776023adb57
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-05-01 12:20:33 +0530

    Time: 7 ms (10.17%), Space: 42.5 MB (31.21%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -5,11 +5,6 @@ class Solution {
         Stack<Character> st_t = new Stack<>();
         
         for(int i = 0; i<s.length(); i++){
-            // if(s.charAt(i) == '#' && !st_s.isEmpty()){
-            //     st_s.pop();
-            // }else{
-            //     st_s.push(s.charAt(i));
-            // }
             if(s.charAt(i) == '#' && !st_s.isEmpty()){
                 st_s.pop();
             }
