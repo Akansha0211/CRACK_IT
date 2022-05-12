070f61425d38ed42b1a2348a2ec8981fb8c27bd3
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-05-12 22:52:49 +0530

    Time: 3 ms (15.41%), Space: 42.3 MB (75.01%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,14 +1,16 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        return minCostHelper(cost.length, cost, map);
+        return Math.min(minCostHelper(0, cost, map), minCostHelper(1, cost, map));
     }
-    public int minCostHelper(int n, int[] cost, HashMap<Integer,Integer> map){
-        if(n==1 || n==0)return 0;
-        if(map.containsKey(n))return map.get(n);
-        int choice1 = cost[n-1] + minCostHelper(n-1, cost, map);
-        int choice2 = cost[n-2] + minCostHelper(n-2, cost, map);
-        map.put(n, Math.min(choice1, choice2));
-        return Math.min(choice1, choice2);
+    public int minCostHelper(int index, int[] cost, HashMap<Integer,Integer> map){
+        
+        if(index >= cost.length)return 0;
+        if(map.containsKey(index))return map.get(index);
+        int choice1 = minCostHelper(index+1, cost, map);
+        int choice2 = minCostHelper(index+2, cost, map);
+        
+        map.put(index, cost[index] + Math.min(choice1, choice2));
+        return cost[index] + Math.min(choice1 , choice2);
     }
 }
