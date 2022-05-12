f6c80d9bb4822f865f69c78ccc9ef9490864c3ca
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-05-12 22:02:53 +0530

    Time: 1 ms (9.97%), Space: 41.7 MB (5.25%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -5,6 +5,7 @@ class Solution {
     }
     public int climbStairsHelper(int n , HashMap<Integer,Integer> map){
         if(n== 0|| n==1)return 1;
+        if(n==2)return 2;
         if(map.containsKey(n))return map.get(n);
         int choice1 = climbStairsHelper(n-1, map);
         int choice2 = climbStairsHelper(n-2, map);
