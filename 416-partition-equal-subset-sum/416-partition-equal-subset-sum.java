78efadb74c19b9800d6e5d884557262387985ee4
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-05-13 11:15:37 +0530

    Time: 111 ms (20.56%), Space: 66.2 MB (26.50%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,28 +1,32 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        //totalSum is odd --> then equal partition is not possible
-        int totalSum = 0;
-        for(int el : nums){
-            totalSum+=el;
+        int sum = 0;
+        for(int i = 0; i<nums.length; i++){
+            sum+= nums[i];
         }
-        if(totalSum %2 != 0)return false;
-        Map<String,Boolean> map = new HashMap<>();
-        return canPartitionHelper(nums,0, totalSum/2, map);
+        if(sum %2 != 0)return false;
+        HashMap<String, Boolean> map = new HashMap<>();
+        return canPartitionHelper(nums,0, sum/2, map);
+        
     }
-    public boolean canPartitionHelper(int[] nums, int index, int target,Map<String,Boolean> map ){
-        if(target == 0) return true;
-        if(index>=nums.length && target != 0)return false;
-        String key = Integer.toString(index)+"#" + Integer.toString(target);
+    public boolean canPartitionHelper(int[] nums, int index, int target, HashMap<String, Boolean> map){
+        
+        if(target == 0)return true;
+        if(index >= nums.length && target != 0)return false;
+        
+        String key = index + "#" + target;
         if(map.containsKey(key))return map.get(key);
-        boolean inclusion = false;
-        if(nums[index]<=target){
-            inclusion = canPartitionHelper(nums, index+1, target- nums[index], map);
+        
+        boolean choose = false;
+        if(nums[index] <= target){
+            choose = canPartitionHelper(nums, index+1 , target - nums[index], map);
         }
-        boolean exclusion = false;
-        if(inclusion == false){
-            exclusion = canPartitionHelper(nums, index+1, target, map);
+        boolean dont = false;
+        if(choose == false){
+            dont = canPartitionHelper(nums, index + 1 , target, map);
         }
-        map.put(key, (inclusion || exclusion));
-        return (inclusion || exclusion);
+        map.put(key, (choose || dont));
+        
+        return (choose || dont);
     }
 }
