333df466657e528b91ff0ddfd1ab781379e43ca0
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-05-31 20:00:08 +0530

    Time: 3 ms (48.42%), Space: 41.9 MB (53.77%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else if(ch == ')'){
+                boolean val  = handleClosing(st, '(');
+                if(val == false)return false;
+            }
+            else if(ch == '}'){
+                boolean val = handleClosing(st, '{');
+                if(val == false)return false;
+            }
+            else if(ch == ']'){
+                boolean val = handleClosing(st, '[');
+                if(val == false)return false;
+            }           
+        }
+        
+         if(st.isEmpty()){
+            return true;
+         }
+         else return false;
+            
+    }
+    public boolean handleClosing(Stack<Character> st, Character needAtTop){
+        if(st.isEmpty()){
+            return false;
+        }
+        else if(st.peek() != needAtTop){
+            return false;
+        }else{
+            st.pop();
+            return true;
+        }
+    }
+}
