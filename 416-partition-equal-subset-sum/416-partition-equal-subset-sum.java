69bf3ecdbc66620621ecb1b112a9a07a97d0b5bc
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-06-03 15:42:35 +0530

    Time: 126 ms (17.98%), Space: 66.3 MB (26.01%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,32 +1,32 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int i = 0; i<nums.length; i++){
+        for(int i=0 ; i<nums.length ; i++){
             sum+= nums[i];
         }
         if(sum %2 != 0)return false;
-        HashMap<String, Boolean> map = new HashMap<>();
-        return canPartitionHelper(nums,0, sum/2, map);
-        
+        HashMap<String,Boolean> map = new HashMap<>();
+        return subsetSumTargetHelper(0, sum/2, nums, map);
     }
-    public boolean canPartitionHelper(int[] nums, int index, int target, HashMap<String, Boolean> map){
+    public boolean subsetSumTargetHelper(int ind, int target, int[] nums, HashMap<String,Boolean> map){
         
         if(target == 0)return true;
-        if(index >= nums.length && target != 0)return false;
+        if(ind >= nums.length && target != 0)return false;
         
-        String key = index + "#" + target;
+        String key = ind + "#" + target;
         if(map.containsKey(key))return map.get(key);
         
-        boolean choose = false;
-        if(nums[index] <= target){
-            choose = canPartitionHelper(nums, index+1 , target - nums[index], map);
+        boolean take = false;
+        if(target >= nums[ind]){
+            take = subsetSumTargetHelper(ind+1, target - nums[ind] , nums, map);
+        } 
+        boolean notTake = false;
+        if(take == false){
+            notTake = subsetSumTargetHelper(ind+1, target, nums, map);
         }
-        boolean dont = false;
-        if(choose == false){
-            dont = canPartitionHelper(nums, index + 1 , target, map);
-        }
-        map.put(key, (choose || dont));
-        
-        return (choose || dont);
+                 
+        map.put(key, notTake | take);
+        return notTake | take;
     }
+    
 }
