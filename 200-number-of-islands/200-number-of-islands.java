86d69f1d18de44d9352e8eee06723a40934b70a4
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-06-10 20:59:38 +0530

    Time: 5 ms (49.87%), Space: 56.5 MB (61.84%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,29 +1,31 @@
 class Solution {
     public int numIslands(char[][] grid) {
         boolean[][] vis = new boolean[grid.length][grid[0].length];
+        
         int islands = 0;
         for(int i = 0; i<grid.length; i++){
             for(int j = 0; j<grid[0].length; j++){
-                if(grid[i][j] == '1' && vis[i][j] ==false){
-                    dFSonMatrix(i, j,grid.length, grid[0].length, vis, grid);
+                if(grid[i][j] == '1' && vis[i][j] == false){
+                    dfsOnMatrix(i, j, vis, grid);
                     islands++;
                 }
             }
         }
         return islands;
     }
-    public void dFSonMatrix(int i, int j, int rows, int cols, boolean[][] vis, char[][] grid){
-        if(i<0 || j<0 || i>=rows || j>= cols || grid[i][j] =='0' || vis[i][j] == true){
+    public void dfsOnMatrix(int i, int j , boolean[][] vis, char[][] grid){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || vis[i][j] == true || grid[i][j] == '0'){
             return;
         }
         vis[i][j] = true;
-        //LEFT
-        dFSonMatrix(i, j-1, rows, cols, vis, grid);
-        //RIGHT
-        dFSonMatrix(i, j+1, rows, cols, vis, grid);
-        //DOWN
-        dFSonMatrix(i+1, j, rows, cols, vis, grid);
-        //UP
-        dFSonMatrix(i-1, j, rows, cols, vis, grid);
+        // left
+        dfsOnMatrix(i, j-1, vis, grid);
+        // right
+        dfsOnMatrix(i, j+1, vis, grid);
+        // up
+        dfsOnMatrix(i-1, j, vis, grid);
+        //down
+        dfsOnMatrix(i+1, j, vis, grid);
+        
     }
 }
