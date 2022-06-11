8e1d1a6a33170a12a37f3a685b942c260b2a4b05
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-06-11 15:03:41 +0530

    Time: 29 ms (21.98%), Space: 71.9 MB (56.95%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,12 +1,14 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
+        
         int n = mat.length;
         int m = mat[0].length;
-        Queue<Integer> q = new LinkedList<>();
-        int[][] ans = new int[n][m];
         
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
+        int[][] ans = new int[mat.length][mat[0].length];
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i= 0; i<mat.length; i++){
+            for(int j = 0; j<mat[0].length; j++){
                 if(mat[i][j] == 0){
                     q.add(i*m+j);
                     ans[i][j] = 0;
@@ -17,16 +19,19 @@ class Solution {
         int level = 0;
         int[][] dir = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
         
+        
+        
         while(!q.isEmpty()){
             int size = q.size();
-            while(size -- >0){
+            while(size-- > 0){
                 int currCell = q.poll();
                 int r = currCell /m;
                 int c = currCell %m;
+                
                 for(int k = 0; k<dir.length; k++){
                     int nr = r + dir[k][0];
                     int nc = c + dir[k][1];
-                    if(nr >=0 && nc >=0 && nr <n && nc <m && ans[nr][nc] == -1){
+                    if(nr >= 0 && nc >=0 && nr <n && nc <m  && ans[nr][nc]== -1){
                         q.add(nr*m + nc);
                         ans[nr][nc] = level + 1;
                     }
@@ -34,8 +39,7 @@ class Solution {
             }
             level++;
         }
-        
-        
         return ans;
+        
     }
 }
