0a012b7f17bf9fe9e3d53a2231307c4814cb6243
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-06-11 23:07:55 +0530

    Time: 1 ms (83.12%), Space: 43.6 MB (54.44%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,25 +1,22 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        // find dip
+        
         int i = nums.length-2;
-        while(i>=0 && nums[i]>=nums[i+1])i--;
-        //if you found dip
+        while(i>=0 && nums[i] >= nums[i+1])i--;
+        // if valid i i.e dip found , we need to swap 
         if(i>=0){
-            // find just greater element
             int j = nums.length-1;
-            while(nums[j]<=nums[i])j--;
-            // swap both the indexes (dip , next greater)
-            swap(i, j,nums);
+            while(nums[j] <= nums[i])j--;
+            swap(nums, i, j);
         }
-        // reverse
         reverse(nums, i+1, nums.length-1);
     }
-    public void swap(int i , int j , int[]nums){
+    public void swap(int[] nums, int i , int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
     public void reverse(int[] nums, int i, int j){
-        while(i<j)swap(i++, j--, nums);
+        while(i<j)swap(nums, i++, j--);
     }
 }
