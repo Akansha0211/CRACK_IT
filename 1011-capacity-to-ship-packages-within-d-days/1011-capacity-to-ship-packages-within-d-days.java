18cceff6859ecdb445d8db58bad11d51ccc4aeed
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-06-19 17:00:07 +0530

    Time: 13 ms (50.72%), Space: 53.9 MB (60.07%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -3,31 +3,35 @@ class Solution {
         int max = Integer.MIN_VALUE;
         int sum = 0;
         for(int i : weights){
-            sum+= i;
+            sum+=i;
             max = Math.max(max, i);
         }
-        
         int lo = max;
         int hi = sum;
         int ans = 0;
+        
         while(lo<=hi){
-            int mid = (lo + hi)/2;
+            int mid = (lo+hi)/2;
             int reqDays = 1;
             int wt = 0;
+            
             for(int i : weights){
-                if(wt+ i <= mid){
-                    wt+= i;
+                if(wt+i <= mid){
+                    wt+=i;
                 }else{
-                    wt = i;
+                    wt= i;
                     reqDays++;
                 }
             }
+            
             if(reqDays <= days){
+                // valid ans toh hain but load aur reduce krke dekhlo..
                 ans = mid;
                 hi = mid-1;
             }else{
                 lo = mid+1;
             }
+            
         }
         return ans;
     }
