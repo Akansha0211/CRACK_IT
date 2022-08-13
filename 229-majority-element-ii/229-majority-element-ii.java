244f783b92ec1b92cae0ed08c79c217d162a227a
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-08-13 23:28:00 +0530

    Time: 3 ms (82.72%), Space: 49.6 MB (66.00%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -4,13 +4,9 @@ class Solution {
         int count1 = 1;
         int val2 = nums[0];
         int count2 = 0;
-        
         for(int i = 1; i<nums.length; i++){
-            if(val1 == nums[i]){
-                count1++;
-            }else if(val2 == nums[i]){
-                count2++;
-            }
+            if(nums[i] == val1)count1++;
+            else if(nums[i] == val2)count2++;
             else{
                 if(count1 == 0){
                     val1 = nums[i];
@@ -19,8 +15,8 @@ class Solution {
                     val2 = nums[i];
                     count2 = 1;
                 }else{
-                    count1--;
-                    count2--;
+                    count1 --;
+                    count2 --;
                 }
             }
         }
@@ -31,9 +27,8 @@ class Solution {
             if(nums[i] == val1)count1++;
             else if(nums[i] == val2)count2++;
         }
-        if(count1>nums.length/3)ans.add(val1);
+        if(count1 > nums.length/3)ans.add(val1);
         if(count2 > nums.length/3)ans.add(val2);
-        
         return ans;
     }
 }
