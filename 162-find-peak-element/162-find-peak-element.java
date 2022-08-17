1ae7ef5a9dd198111e544e01bd0f985e501b19f5
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-08-17 17:29:10 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (98.21%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -3,10 +3,6 @@ class Solution {
         if(nums.length == 1) return 0; // single element
         
         int n = nums.length;
-        
-		// check if 0th/n-1th index is the peak element
-        if(nums[0] > nums[1]) return 0;
-        if(nums[n-1] > nums[n-2]) return n-1;
 		
 		// search in the remaining array
         int start = 1;
@@ -18,6 +14,9 @@ class Solution {
             else if(nums[mid] < nums[mid-1]) end = mid - 1;
             else if(nums[mid] < nums[mid+1]) start = mid + 1;
         }
+        // check if 0th/n-1th index is the peak element
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
         return -1; // dummy return statement
     }
 }
