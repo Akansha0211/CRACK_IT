313955b5384ecf3b5629d0113d97f812d199d57f
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-08-18 12:34:57 +0530

    Time: 15 ms (80.16%), Space: 193.8 MB (46.98%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,23 +1,24 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> ans = new ArrayList<>();
-        partitionHelper(0, s, new ArrayList<>(), ans);
+        partitionHelper(s, 0, new ArrayList<String>(), ans);
         return ans;
     }
-    public static void partitionHelper(int ind, String s, List<String> ds, List<List<String>> ans){
-        if(ind == s.length()){
-            ans.add(new ArrayList<>(ds));
+    public void partitionHelper(String s,int index, List<String> ds, List<List<String>> ans){
+        if(index == s.length()){
+            ans.add(new ArrayList<String>(ds));
             return;
         }
-        for(int i = ind; i<s.length(); i++){
-            if(isPalindrome(s, ind, i)){
-                ds.add(s.substring(ind, i+1));
-                partitionHelper(i+1, s, ds, ans);
+        for(int i = index; i<s.length(); i++){
+            if(isValid(s, index, i)){
+                ds.add(s.substring(index, i+1));
+                partitionHelper(s, i+1, ds, ans);
                 ds.remove(ds.size()-1);
+                
             }
         }
     }
-    public static boolean isPalindrome(String s, int start, int end){
+    public boolean isValid(String s, int start, int end){
         while(start<=end){
             if(s.charAt(start) != s.charAt(end)){
                 return false;
