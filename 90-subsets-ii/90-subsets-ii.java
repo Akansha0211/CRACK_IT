02fd67020ae8f145a6a3cdeafab708c7f5653ea8
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-08-20 15:45:56 +0530

    Time: 3 ms (48.39%), Space: 44.1 MB (52.65%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,16 +1,16 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        subsetHelper(0, nums, new ArrayList<>(), ans);
-        return ans;
+        Arrays.sort(nums);
+        subsetsHelper(0,nums, new ArrayList<>(), ans);
+        return ans;      
     }
-    public static void subsetHelper(int ind, int[] arr, List<Integer> ds, List<List<Integer>> ans){
+    public void subsetsHelper(int index, int[] nums, ArrayList<Integer> ds, List<List<Integer>> ans){
         ans.add(new ArrayList<>(ds));
-        for(int i = ind; i<arr.length; i++){
-            if(i>ind && arr[i] == arr[i-1])continue;
-            ds.add(arr[i]);
-            subsetHelper(i+1, arr, ds, ans);
+        for(int i = index; i<nums.length; i++){
+            if(i!=index && nums[i] == nums[i-1])continue;
+            ds.add(nums[i]);
+            subsetsHelper(i+1, nums, ds, ans);
             ds.remove(ds.size()-1);
         }
     }
