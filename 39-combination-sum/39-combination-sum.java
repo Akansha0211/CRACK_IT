4687801abc5a9a4684fdbe6bf07671c4498e2616
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-08-20 20:14:58 +0530

    Time: 3 ms (95.48%), Space: 45.7 MB (37.35%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
         combinationHelper(0, target, candidates, new ArrayList<Integer>(), ans);
         return ans;
     }
@@ -11,13 +12,18 @@ class Solution {
         }
         if(index>=candidates.length)return;
         
+        // added this line to avoid more unnecessary calls
+        if(candidates[index]> target)return;
         
         //pick
-        if(candidates[index]<=target){
-            ds.add(candidates[index]);
-            combinationHelper(index, target - candidates[index],candidates, ds, ans);
-            ds.remove(ds.size()-1);
-        }
+        // if(candidates[index]<=target){
+        //     ds.add(candidates[index]);
+        //     combinationHelper(index, target - candidates[index],candidates, ds, ans);
+        //     ds.remove(ds.size()-1);
+        // }
+        ds.add(candidates[index]);
+        combinationHelper(index, target - candidates[index],candidates, ds, ans);
+        ds.remove(ds.size()-1);
         // do not pick
         combinationHelper(index+1, target, candidates, ds, ans);
     }
