4d3a8f443154c01b805d0663b58d4a50b0de6883
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-08-21 15:52:02 +0530

    Time: 35 ms (79.97%), Space: 50.5 MB (93.98%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -2,10 +2,8 @@ class Solution {
     class Pair implements Comparable<Pair>{
         int index;
         int distance;
-        Pair(){
-            
-        }
-        Pair(int index,int distance){
+        //Pair(){}
+        Pair(int index, int distance){
             this.index = index;
             this.distance = distance;
         }
@@ -13,14 +11,14 @@ class Solution {
             return this.distance - o.distance;
         }
     }
-    
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>(Collections.reverseOrder());
+        // we need max priority Queue ( as we want k amllest elements.
+        PriorityQueue<Pair> pq = new PriorityQueue<>(Collections.reverseOrder()); // max pq
         for(int i = 0; i<points.length; i++){
-            int xCord = points[i][0];
-            int yCord = points[i][1];
-            int distance = (xCord * xCord) + (yCord* yCord);
-            if(pq.size() <k){
+            int x = points[i][0];
+            int y = points[i][1];
+            int distance = (x*x) + (y*y);
+            if(pq.size()<k){
                 pq.add(new Pair(i, distance));
             }else{
                 if(pq.peek().distance > distance){
@@ -32,7 +30,7 @@ class Solution {
         int[][] ans = new int[k][points[0].length];
         int i = 0;
         while(pq.size()>0){
-            Pair rem = pq.remove();
+            Pair rem = pq.poll();
             int index = rem.index;
             ans[i] = points[index];
             i++;
