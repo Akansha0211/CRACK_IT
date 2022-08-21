8a7d7d67ac11c8f074bf1b25fa52caa27304e9b6
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-08-21 20:19:32 +0530

    Time: 5 ms (72.16%), Space: 49.4 MB (23.06%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
@@ -1,5 +1,27 @@
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        // int ans = 0;
+        // for(int i = 0; i<stations.length; i++){
+        //     int position = stations[i][0];
+        //     int fuel = stations[i][1];
+        //     // startFuel == max position you can reach
+        //     if(startFuel < position){
+        //         if(pq.isEmpty())return -1;
+        //         startFuel+=pq.poll();
+        //         ans++;
+        //     }
+        //     pq.add(fuel); // when psoition can be reached
+        // }
+        // // after crossing the whole array the target is not reached 
+        // // that means target = 130 and last position of array = 60
+        // // [10,60],[20,30],[30,30],[60,40]
+        // while(startFuel < target){
+        //     if(pq.isEmpty())return -1;
+        //     startFuel+=pq.poll();
+        //     ans++;
+        // }
+        // return ans;
         
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int ans = 0;
