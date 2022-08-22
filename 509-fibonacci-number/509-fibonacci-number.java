6803dc4f9e8f3cedd7857dd46c5a6da134c704c2
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-08-22 13:49:53 +0530

    Time: 12 ms (23.04%), Space: 41.6 MB (6.20%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,12 +1,12 @@
 class Solution {
     public int fib(int n) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        return fibHelper(n, map);
+        return fibHelper(n);
     }
-    public int fibHelper(int n, HashMap<Integer,Integer> map){
-        if(n == 0|| n==1)return n;
-        if(map.containsKey(n))return map.get(n);
-        map.put(n, fibHelper(n-1, map)+fibHelper(n-2, map));
-        return map.get(n);
+    public int fibHelper(int n){
+        if(n== 0)return 0;
+        if(n==1)return 1;
+        int choice1 = fibHelper(n-1);
+        int choice2 = fibHelper(n-2);
+        return choice1 + choice2;
     }
 }
