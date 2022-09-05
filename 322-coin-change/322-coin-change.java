bb49d9fa6add661c3ef0cc9b505f2502a2931937
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-09-05 09:08:45 +0530

    Time: 1616 ms (5.03%), Space: 58.6 MB (10.65%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,21 +1,22 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        Map<String,Integer> map = new HashMap<>();
-        long ans = minCoins(coins, 0, amount, map);
-        if(ans >= Integer.MAX_VALUE)return -1;
+        HashMap<String,Integer> map = new HashMap<>();
+       long ans = coinChangeHelper(0, amount, coins, map);
+        if(ans>=Integer.MAX_VALUE)return -1;
         return (int)ans;
+        
     }
-    public long minCoins(int[] coins, int index, int amount, Map<String,Integer> map){
-        if(amount ==0)return 0;
-        if(index >= coins.length)return Integer.MAX_VALUE;
-        String key = Integer.toString(index) + "#" + Integer.toString(amount);
+    public long coinChangeHelper(int index, int amount, int[] coins, HashMap<String,Integer> map){
+        if(amount == 0)return 0;
+        if(index>=coins.length)return Integer.MAX_VALUE;
+        String key = Integer.toString(index) + "#" + Integer.toString(amount); 
         if(map.containsKey(key))return map.get(key);
-        long consider = Integer.MAX_VALUE;
-        if(coins[index] <= amount){
-            consider = 1 + minCoins(coins, index, amount - coins[index], map);
+        long take = Integer.MAX_VALUE;
+        if(amount>= coins[index]){
+            take = 1+ coinChangeHelper(index, amount-coins[index], coins, map);
         }
-        long notConsider = minCoins(coins, index+1, amount, map);
-        map.put(key, (int)Math.min(consider, notConsider));
-        return Math.min(consider, notConsider);
+        long dontTake = coinChangeHelper(index+1, amount, coins, map);
+        map.put(key, (int)Math.min(take, dontTake));
+        return Math.min(take, dontTake);
     }
 }
