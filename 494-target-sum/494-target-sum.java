f421622ba1cc30ed2de140936e53711c5326d8fb
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-09-05 11:55:34 +0530

    Time: 812 ms (17.38%), Space: 42.2 MB (64.99%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,16 +1,12 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        HashMap<String,Integer> map = new HashMap<>();
-        return targetSumHelper(0, target, nums, map);
+        return findWaysHelper(0, target, nums);
     }
-    public int targetSumHelper(int index, int target, int[] nums, HashMap<String, Integer> map){
-        if(index >= nums.length && target == 0)return 1;
+    public int findWaysHelper(int index, int target, int[]nums){
+        if(index>=nums.length && target == 0)return 1;
         if(index >= nums.length && target != 0)return 0;
-        String key = index + "#" + target;
-        if(map.containsKey(key))return map.get(key);
-        int plus = targetSumHelper(index+1, target - nums[index], nums, map);
-        int neg  = targetSumHelper(index+1, target + nums[index], nums, map);
-        map.put(key, plus + neg);
-        return (plus + neg);
+        int plus = findWaysHelper(index+1, target-nums[index], nums);
+        int neg  =  findWaysHelper(index+1, target + nums[index], nums); 
+        return plus + neg;
     }
 }
