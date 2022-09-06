d4635099679536c2dae3724817f572917def9fab
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-09-06 15:02:17 +0530

    Time: 1457 ms (5.07%), Space: 296.6 MB (5.10%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,28 +1,19 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        // HashMap<String, Integer> map = new HashMap<>();
-        int [][] dp = new int[text1.length()+1][text2.length()+1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-        return lcsHelper(text1, text2, 0,0, dp);
+        HashMap<String,Integer> map = new HashMap<>();
+        return lcsHelper(0,0, text1, text2, map);
     }
-    public int lcsHelper(String s1, String s2, int i , int j, int[][] dp){
-        if(i>=s1.length() || j>=s2.length())return 0;
-        if(dp[i][j] !=-1)return dp[i][j];
+    public int lcsHelper(int i ,int j, String s1, String s2, HashMap<String,Integer> map){
+        
+        if(i>=s1.length() || j>= s2.length())return 0;
+        String key = Integer.toString(i) + "#" + Integer.toString(j);
+        if(map.containsKey(key))return map.get(key);
         if(s1.charAt(i) == s2.charAt(j)){
-            // map.put(key, 1+ lcsHelper(s1, s2, i+1, j+1, map));
-            return dp[i][j] = 1+ lcsHelper(s1, s2, i+1, j+1, dp);
-            // return map.get(key);
+            return 1 + lcsHelper(i+1, j+1, s1, s2, map);
         }
-        else{
-            int choice1 = lcsHelper(s1, s2, i, j+1, dp);
-            int choice2 = lcsHelper(s1, s2, i+1, j, dp);
-            // map.put(key, Math.max(choice1, choice2));
-            // return Math.max(choice1, choice2);
-            // return map.get(key);
-            return dp[i][j]  =  Math.max(choice1, choice2);
-            
-        }
+        int choice1 = lcsHelper(i, j+1, s1, s2, map);
+        int choice2 = lcsHelper(i+1, j, s1, s2, map);
+        map.put(key, Math.max(choice1, choice2));
+        return Math.max(choice1, choice2);
     }
 }
