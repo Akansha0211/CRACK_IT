f5ba148cf90826caf9a063b303c89f346d7d7798
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-09-06 10:40:03 +0530

    Time: 1003 ms (5.03%), Space: 250.7 MB (5.03%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,19 +1,19 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        Map<String, Integer> map = new HashMap<>();
-        return totalCombinations(coins, 0, amount, map);
+        HashMap<String,Integer> map = new HashMap<>();
+        return changeHelper(0, amount, coins, map);
     }
-    public int totalCombinations(int[] coins, int index, int amount, Map<String, Integer> map){
+    public int changeHelper(int index, int amount, int[] coins, HashMap<String,Integer> map){
         if(amount == 0)return 1;
-        if(index>= coins.length)return 0;
+        if(index >= coins.length)return 0;
         String key = Integer.toString(index) + "#" + Integer.toString(amount);
         if(map.containsKey(key))return map.get(key);
-        int consider = 0;
-        if(coins[index] <= amount){
-            consider = totalCombinations(coins, index, amount- coins[index], map);
+        int take = 0;
+        if(amount >= coins[index]){
+            take = changeHelper(index, amount-coins[index], coins, map);
         }
-        int notConsider = totalCombinations(coins, index+1, amount, map);
-        map.put(key, consider + notConsider);
-        return consider + notConsider;
+        int dontTake = changeHelper(index+1, amount, coins, map);
+        map.put(key, (take+dontTake));
+        return take + dontTake;
     }
 }
