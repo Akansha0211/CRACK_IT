bd0d70046d7a46286ba67e35ee22ab3b11b99685
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-09-07 13:34:45 +0530

    Time: 1373 ms (5.02%), Space: 244.8 MB (5.06%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,14 +3,14 @@ class Solution {
         HashMap<String,Integer> map = new HashMap<>();
         int n = text1.length();
         int m = text2.length();
-        return lcsHelper(n-1,m-1, text1, text2, map);
+        return lcsHelper(n,m, text1, text2, map);
     }
     public int lcsHelper(int i ,int j, String s1, String s2, HashMap<String,Integer> map){
         
-        if(i<0 || j<0)return 0;
+        if(i==0 || j==0)return 0;
         String key = Integer.toString(i) + "#" + Integer.toString(j);
         if(map.containsKey(key))return map.get(key);
-        if(s1.charAt(i) == s2.charAt(j)){
+        if(s1.charAt(i-1) == s2.charAt(j-1)){
             return 1 + lcsHelper(i-1, j-1, s1, s2, map);
         }
         int choice1 = lcsHelper(i, j-1, s1, s2, map);
