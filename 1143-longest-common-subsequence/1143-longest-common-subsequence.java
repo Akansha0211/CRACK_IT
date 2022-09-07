ea91c4e25b77ba744fa7389c85a588ec0fdf4e7b
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-09-07 15:05:47 +0530

    Time: 16 ms (81.29%), Space: 46.3 MB (66.12%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,9 +1,27 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        HashMap<String,Integer> map = new HashMap<>();
-        int n = text1.length();
-        int m = text2.length();
-        return lcsHelper(n,m, text1, text2, map);
+    public int longestCommonSubsequence(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n+1][m+1];
+        
+        for(int j = 0; j<=m; j++) dp[0][j] = 0;
+        for(int i = 0; i<=n; i++) dp[i][0] = 0;
+        
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m ; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] =  1 + dp[i-1][j-1];
+                }
+                else{
+                    int choice1 = dp[i][j-1];
+                    int choice2 = dp[i-1][j];
+                
+                    dp[i][j] =  Math.max(choice1, choice2);
+                }
+            }
+        }
+        return dp[n][m];
+        
     }
     public int lcsHelper(int i ,int j, String s1, String s2, HashMap<String,Integer> map){
         
@@ -18,4 +36,5 @@ class Solution {
         map.put(key, Math.max(choice1, choice2));
         return Math.max(choice1, choice2);
     }
+    
 }
