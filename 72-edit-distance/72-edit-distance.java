aae61d821b32ce0b38cbcf48ece73feba86f6379
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-09-12 08:24:50 +0530

    Time: 4 ms (97.69%), Space: 42.4 MB (93.36%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,27 +1,24 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        HashMap<String,Integer> map = new HashMap<>();
-        return minDistanceHelper(word1, word2, 0,0, map);
-    }
-    public int minDistanceHelper(String word1, String word2, int i , int j, HashMap<String,Integer> map){
-        if(word1.length() ==0)return word2.length();
-        if(word2.length() ==0)return word1.length();
-        
-        if(i >= word1.length())return word2.length() - j;
-        if(j >= word2.length())return word1.length() -i;
-        
-        String key = i + "#" + j;
-        if(map.containsKey(key))return map.get(key);
-        if(word1.charAt(i) == word2.charAt(j)){
-            map.put(key, minDistanceHelper(word1, word2, i+1, j+1, map));
-            return minDistanceHelper(word1, word2, i+1, j+1, map);
+        int[][] dp = new int[word1.length()][word2.length()];
+        for(int i = 0; i<dp.length; i++){
+            for(int j = 0; j<dp[0].length; j++){
+                dp[i][j] = -1;
+            }
         }
-        int insert = minDistanceHelper(word1, word2, i, j+1, map);
-        int delete = minDistanceHelper(word1, word2, i+1, j, map);
-        int replace = minDistanceHelper(word1, word2, i+1, j+1, map);
+        return minOpnHelper(0, 0, word1, word2, dp);
+    }
+    public static int minOpnHelper(int i, int j, String s1, String s2, int[][] dp){
         
-        map.put(key, 1 + Math.min(replace, Math.min(insert, delete)));
-        
-        return 1 + Math.min(replace, Math.min(insert, delete));
+        if(i>=s1.length())return s2.length() - j;
+        if(j>=s2.length())return s1.length() - i;
+        if(dp[i][j] != -1)return dp[i][j];
+        if(s1.charAt(i) == s2.charAt(j)){
+            return dp[i][j] = minOpnHelper(i+1, j+1, s1, s2, dp);
+        }
+        int insertion = 1 + minOpnHelper(i, j+1, s1, s2, dp);
+        int deletion = 1 + minOpnHelper(i+1, j, s1, s2, dp);
+        int replace = 1 + minOpnHelper(i+1, j+1, s1, s2, dp);
+        return dp[i][j] =  Math.min(insertion, Math.min(deletion,replace));
     }
 }
