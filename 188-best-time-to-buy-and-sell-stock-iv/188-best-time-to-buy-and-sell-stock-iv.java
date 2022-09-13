2374262070b08b50e9e5e4c8b923c1aae3ed52ea
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-09-13 11:27:56 +0530

    Time: 471 ms (5.03%), Space: 91.2 MB (5.06%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        HashMap<String,Integer> map = new HashMap<>();
+        return maxProfitHelper(prices, 0, 1, k, map);
+    }
+    public int maxProfitHelper(int[] prices, int index, int canBuy, int tranCount,HashMap<String,Integer> map){
+        if(index >= prices.length || tranCount == 0)return 0;
+        String key = index + "#" + canBuy + "#" + tranCount;
+        if(map.containsKey(key))return map.get(key);
+        if(canBuy == 1){
+            int idle = maxProfitHelper(prices, index+1, canBuy, tranCount, map);
+            int buy = - prices[index] +  maxProfitHelper(prices, index+1, 0, tranCount, map);
+            map.put(key, Math.max(idle, buy));
+            return Math.max(idle,buy);
+        }else{
+            int idle = maxProfitHelper(prices, index+1, canBuy, tranCount, map);
+            int sell = + prices[index] + maxProfitHelper(prices, index+1, 1, tranCount -1, map);
+            map.put(key, Math.max(idle,sell));
+            return Math.max(idle, sell);
+        }        
+    }
+}
