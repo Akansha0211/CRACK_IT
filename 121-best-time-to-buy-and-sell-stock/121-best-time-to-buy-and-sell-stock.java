5b331ec13ee22d78675cbebfabb80fd20488d5d3
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-09-13 10:54:17 +0530

    Time: 1536 ms (6.04%), Space: 355.3 MB (5.08%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min = prices[0];
-        int maxProfit = 0;
-        
-        for(int i = 1; i<prices.length; i++){
-            if(prices[i] < min){
-                min = prices[i];
-            }else{
-                maxProfit = Math.max(maxProfit, prices[i]- min);
-            }
-        }
-        return maxProfit;
+        HashMap<String,Integer> map = new HashMap<>();
+        return maxProfitHelper(prices, 0, 1, 1, map);
+    }
+    public int maxProfitHelper(int[] prices, int index, int canBuy, int tranCount,HashMap<String,Integer> map){
+        if(index >= prices.length || tranCount == 0)return 0;
+        String key = index + "#" + canBuy + "#" + tranCount;
+        if(map.containsKey(key))return map.get(key);
+        if(canBuy == 1){
+            int idle = maxProfitHelper(prices, index+1, canBuy, tranCount, map);
+            int buy = - prices[index] +  maxProfitHelper(prices, index+1, 0, tranCount, map);
+            map.put(key, Math.max(idle, buy));
+            return Math.max(idle,buy);
+        }else{
+            int idle = maxProfitHelper(prices, index+1, canBuy, tranCount, map);
+            int sell = + prices[index] + maxProfitHelper(prices, index+1, 1, tranCount -1, map);
+            map.put(key, Math.max(idle,sell));
+            return Math.max(idle, sell);
+        }        
     }
 }
