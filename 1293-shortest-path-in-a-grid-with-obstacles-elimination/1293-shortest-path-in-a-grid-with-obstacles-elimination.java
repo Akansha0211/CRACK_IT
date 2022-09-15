aea2177a427929d6757a63c8069a8914f4c2152b
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-09-16 00:51:48 +0530

    Time: 25 ms (68.72%), Space: 49 MB (50.31%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,51 +1,44 @@
 class Solution {
-    class Node{
-        int currRow;
-        int currCol;
-        int remSuperPower;
-        
-        public Node(int r, int c, int s){
-            currRow = r;
-            currCol = c;
-            remSuperPower = s;
+    class Pair{
+        int row;
+        int col;
+        int remPower;
+        Pair(int row, int col, int remPower){
+            this.row = row;
+            this.col = col;
+            this.remPower = remPower;
         }
     }
-    public int shortestPath(int[][] grid, int k) {
+    public int shortestPath(int[][] grid, int k) {          
         int n = grid.length;
         int m = grid[0].length;
-        if(n==1 && m ==1)return 0;
-        
-        int maxRemSuperPower[][] = new int[n][m];
-        for(int []row : maxRemSuperPower){
+        if(n==1 && m==1)return 0;
+        int[][] maxPower = new int[n][m];
+        for(int[] row : maxPower){
             Arrays.fill(row, -1);
         }
         
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(0,0,k));
-        maxRemSuperPower[0][0] = k;
-        
-        int level= 0;
-        int dir[][] = new int[][]{{0,-1}, {0,1},{-1,0},{1,0}};
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(0,0, k));
+        maxPower[0][0] = k;
+        int[][] dir = new int[][]{{0,1},{0,-1},{-1,0},{1,0}};
+        int level = 0;
         while(!q.isEmpty()){
             int size = q.size();
-            while(size-- >0){
-                Node currNode = q.poll();
-                for(int i = 0;i<dir.length; i++){
-                    int nextNodeRow = currNode.currRow + dir[i][0];
-                    int nextNodeCol = currNode.currCol + dir[i][1];
-                    if(nextNodeRow == n-1 && nextNodeCol == m-1){
-                        return level+1;
-                    }
-                    
-                    if(nextNodeRow >=0 && nextNodeRow<n && nextNodeCol>=0 && nextNodeCol<m){ // within matrix bounds        
-                        if(grid[nextNodeRow][nextNodeCol] == 1 && currNode.remSuperPower >0 && maxRemSuperPower[nextNodeRow][nextNodeCol] < (currNode.remSuperPower-1)){
-                            q.add(new Node(nextNodeRow, nextNodeCol, currNode.remSuperPower-1));
-                            maxRemSuperPower[nextNodeRow][nextNodeCol] = currNode.remSuperPower-1;
-                        }else if(grid[nextNodeRow][nextNodeCol] == 0 && maxRemSuperPower[nextNodeRow][nextNodeCol] < (currNode.remSuperPower)){
-                            q.add(new Node(nextNodeRow, nextNodeCol, currNode.remSuperPower));
-                            maxRemSuperPower[nextNodeRow][nextNodeCol] = currNode.remSuperPower;
+            for(int i = 0; i<size; i++){
+                Pair remPair = q.poll();
+                for(int j = 0; j<dir.length; j++){
+                    int x = remPair.row + dir[j][0];
+                    int y = remPair.col + dir[j][1];
+                    if(x == n-1 && y == m-1)return (level+1);
+                    if((x>=0 && x<n) && (y>=0 && y<m) ){
+                        if(grid[x][y] == 1 && remPair.remPower >0 && maxPower[x][y]<(remPair.remPower-1)){
+                           q.add(new Pair(x,y, remPair.remPower -1));
+                            maxPower[x][y] = remPair.remPower-1; 
+                        }else if(grid[x][y] == 0 && maxPower[x][y] <(remPair.remPower)){
+                            q.add(new Pair(x, y, remPair.remPower));
+                            maxPower[x][y] = remPair.remPower;
                         }
-                        
                     }
                 }
             }
