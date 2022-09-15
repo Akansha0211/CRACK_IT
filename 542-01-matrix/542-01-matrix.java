ae67e4e8632a667975bfdb9fc5ab269de36d7ac2
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-09-15 17:29:14 +0530

    Time: 34 ms (22.03%), Space: 72.1 MB (62.43%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,45 +1,41 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        
-        int n = mat.length;
+        int n = mat.length; 
         int m = mat[0].length;
-        
-        int[][] ans = new int[mat.length][mat[0].length];
+
         Queue<Integer> q = new LinkedList<>();
-        
-        for(int i= 0; i<mat.length; i++){
-            for(int j = 0; j<mat[0].length; j++){
+        int[][] ans = new int[n][m];
+        for(int i = 0; i<n ; i++){
+            for(int j = 0; j<m; j++){
                 if(mat[i][j] == 0){
                     q.add(i*m+j);
-                    ans[i][j] = 0;
-                }else ans[i][j] = -1;
+                    ans[i][j] = 0;                    
+                }else{
+                    ans[i][j] = -1; // marking -1 for unvisited
+                }
             }
         }
         
         int level = 0;
         int[][] dir = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-        
-        
-        
         while(!q.isEmpty()){
             int size = q.size();
-            while(size-- > 0){
-                int currCell = q.poll();
-                int r = currCell /m;
-                int c = currCell %m;
+            for(int i = 0; i<size; i++){
+                int rem  = q.poll();
+                int row = rem/m;
+                int col = rem%m;
                 
                 for(int k = 0; k<dir.length; k++){
-                    int nr = r + dir[k][0];
-                    int nc = c + dir[k][1];
-                    if(nr >= 0 && nc >=0 && nr <n && nc <m  && ans[nr][nc]== -1){
-                        q.add(nr*m + nc);
-                        ans[nr][nc] = level + 1;
+                    int x = row + dir[k][0];
+                    int y = col + dir[k][1];
+                    if((x>=0 && x<n) && (y>=0 && y<m) && ans[x][y] == -1){
+                        q.add(x*m + y);
+                        ans[x][y] = level+1;
                     }
                 }
             }
             level++;
         }
         return ans;
-        
     }
 }
