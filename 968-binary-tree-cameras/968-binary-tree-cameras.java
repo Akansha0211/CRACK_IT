a7b2a7a815f8db552a6e5baca2b2412c589699c9
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-09-27 06:32:24 +0530

    Time: 1 ms (81.28%), Space: 43.2 MB (73.04%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -14,22 +14,18 @@
  * }
  */
 class Solution {
-    int camera = 0;
+    int cameras = 0;
     public int minCameraCover(TreeNode root) {
-        int ans = traversal(root);
-        if(ans == 1){
-            // no parent i.e root == 1(dummy leaf)
-            camera++;
-        }
-        return camera;
+        if(traversal(root) == 1)cameras++;
+        return cameras;
     }
     public int traversal(TreeNode root){
         if(root == null)return 3;
         int left = traversal(root.left);
         int right = traversal(root.right);
         if(left == 1 || right == 1){
-             camera++;
-            return 2;           
+            cameras++;
+            return 2;
         }
         if(left == 2 || right == 2)return 3;
         return 1;
