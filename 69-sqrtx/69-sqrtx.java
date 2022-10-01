bfec2ac39e8ff656dfeace4489efa7605ef46ffe
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-10-01 11:24:56 +0530

    Time: 3 ms (44.27%), Space: 41.8 MB (20.42%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,8 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        // Linear Search solution
-        long y = 1;
-        while(y*y <= x)y++;
-        return (int)y-1;
+        // Binary Search soln
+        long lo = 1;
+        long hi = x;
+        long ans = 0;
+        while(lo<=hi){
+            long mid = lo+(hi-lo)/2;
+            if(mid*mid <= x){
+                ans = mid;
+                lo = mid+1;
+            }else{
+                hi = mid-1;
+            }
+        }
+        return (int)ans;
     }
 }
