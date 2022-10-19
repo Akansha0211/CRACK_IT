ed71e14cf9408991e2ce44515faad11421ad51fb
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-10-19 16:20:40 +0530

    Time: 124 ms (17.81%), Space: 110.1 MB (11.55%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,10 +1,24 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[][] dp = new int[nums.length][nums.length+1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        // int[][] dp = new int[nums.length][nums.length+1];
+        // for(int[] row : dp){
+        //     Arrays.fill(row, -1);
+        // }
+        // return lisHelper(nums, 0, -1, dp);
+        
+        int[][] dp  = new int[nums.length+1][nums.length+1];
+        int n = nums.length;
+        for(int index = n-1; index>=0 ; index--){
+            for(int prevIndex = index-1; prevIndex >=-1; prevIndex--){
+                int notTake = 0 + dp[index+1][prevIndex+1];
+                int take = 0;
+                if(prevIndex == -1 || nums[prevIndex] < nums[index]){
+                    take = 1 + dp[index+1][index+1];
+                }
+                dp[index][prevIndex+1] = Math.max(take, notTake);
+            }
         }
-        return lisHelper(nums, 0, -1, dp);
+        return dp[0][-1+1];
     }
     public int lisHelper(int[] nums, int index, int prevIndex, int[][] dp){
         if(index == nums.length)return 0;
