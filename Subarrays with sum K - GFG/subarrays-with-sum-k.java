a32c28308f93018622b070e61a5708b9ab0979e8
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-12-03 14:14:58 +0530

    Added solution - LeetHub

---
+++ Subarrays with sum K - GFG/subarrays-with-sum-k.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int Arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                Arr[i] = Integer.parseInt(input_line[i]);
+            int k = Integer.parseInt(read.readLine());
+            
+            Solution ob = new Solution();
+            System.out.println(ob.findSubArraySum(Arr, N, k));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int findSubArraySum(int Arr[], int N, int k)
+    {
+        // code here
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        
+        int prefixSum = 0;
+        int count = 0;
+        
+        for(int i = 0; i<Arr.length; i++){
+            prefixSum += Arr[i];
+            if(map.containsKey(prefixSum - k)){
+                count+=map.get(prefixSum-k);
+            }
+            map.put(prefixSum, map.getOrDefault(prefixSum,0)+1);
+        }
+        return count;
+    }
+}
