105950087c853472ec315eeaa462bafc77a5601d
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-12-24 15:49:39 +0530

    Time: 14 ms (73.10%), Space: 44.1 MB (99.42%) - LeetHub

---
+++ 0554-brick-wall/0554-brick-wall.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int leastBricks(List<List<Integer>> wall) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int minCuts = wall.size();
+        int crackDist = 0;
+        int maxCount = 0;
+        for(List<Integer> row : wall){
+            crackDist = 0;
+            for(int i = 0; i<row.size()-1; i++){
+                crackDist += row.get(i);
+                map.put(crackDist, map.getOrDefault(crackDist,0)+1);
+                maxCount = Math.max(maxCount, map.get(crackDist));
+            }
+            minCuts = Math.min(minCuts, wall.size()- maxCount);
+        }
+        return minCuts;
+    }
+}
