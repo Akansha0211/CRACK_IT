12130645f666d5ffed61e1efae3029181890a066
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2022-12-25 14:57:08 +0530

    Added solution - LeetHub

--- Subarrays with sum K - GFG/subarrays-with-sum-k.java
+++ Subarrays with sum K - GFG/subarrays-with-sum-k.java
@@ -34,13 +34,14 @@ class Solution
     {
         // code here
         HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(0,1);
+        // map.put(0,1);
         
         int prefixSum = 0;
         int count = 0;
         
         for(int i = 0; i<Arr.length; i++){
             prefixSum += Arr[i];
+            if(prefixSum == k)count++;
             if(map.containsKey(prefixSum - k)){
                 count+=map.get(prefixSum-k);
             }
