b9f0f01b1132a185a8e768d10de714c01288da42
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2023-01-02 14:13:17 +0530

    Time: 1 ms (100.00%), Space: 40.9 MB (77.90%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -5,31 +5,24 @@ class Solution {
         Character ch0 = word.charAt(0);
         Character ch1 = word.charAt(1);
         
-        if(Character.isLowerCase(ch0)){
+        
+        // LEETCODE
+        if(Character.isUpperCase(ch0)){
+            boolean firstChar = Character.isUpperCase(ch1);
+            // if second character is uppe then whole string will be of capitals
+            // if secomd character is lower than form index = 2 to last will all be lower only
+            for(int i = 2; i<word.length() ;i++){
+                boolean currChar = Character.isUpperCase(word.charAt(i));
+                if(firstChar != currChar)return false;
+            }
+        }else{
+            // if first char i.e index 0 is lower then all should be lower only.
             for(int i = 1; i<word.length(); i++){
                 if(Character.isUpperCase(word.charAt(i))){
                     return false;
                 }
             }
         }
-        
-        // LEETCODE
-        if(Character.isUpperCase(ch0) && Character.isUpperCase(ch1)){
-            for(int i = 2; i<word.length(); i++){
-                if(!Character.isUpperCase(word.charAt(i))){
-                    return false;
-                }
-            }
-        }
-        //Leetcode
-        if(Character.isUpperCase(ch0) && Character.isLowerCase(ch1)){
-            for(int i = 2; i<word.length(); i++){
-                if(Character.isUpperCase(word.charAt(i))){
-                    return false;
-                }
-            }
-        }
         return true;
-        
     }
 }
