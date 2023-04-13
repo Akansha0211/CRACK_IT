50f6524790818349ba4fc44da1f4f58d368c1ad1
Akansha Saxena <49263297+Akansha0211@users.noreply.github.com>
2023-04-13 18:51:00 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (50.44%) - LeetHub

--- 2429-minimize-xor/2429-minimize-xor.java
+++ 2429-minimize-xor/2429-minimize-xor.java
@@ -1,48 +1,27 @@
 class Solution {
     public int minimizeXor(int num1, int num2) {
-        // int count = Integer.bitCount(num2);
-        // int ans = 0;
-        // for(int i = 60; i>=0; i--){
-        //     int currBit = (num1 >> i) &1; // num1 >> i is the current number
-        //     if((currBit == 1) && count >0){
-        //         ans = ans | (1<<i);
-        //         count--;
-        //     }
-        // }
-        // // eg num = 1 , num2 = 12
-        // if(count>0){
-        //     for(int i = 0; i<=60; i++){
-        //         int currBit = (ans>>i)&1;
-        //        // System.out.println(ans>>i);
-        //         if(currBit !=1){
-        //             ans = ans | (1<<i);
-        //             count--;
-        //         }
-        //     }
-        // }
-        // return ans;
-        
-        int bit = Integer.bitCount(num2);
-		
+        int count = Integer.bitCount(num2);
         int ans = 0;
-
-        for(int i=31;i>=0;i--) {
-			int currBit = (num1 >> i) & 1;
-            if(currBit == 1 && bit > 0) {
-				ans |= (1 << i);
-                bit--;
+        
+        for(int i = 31; i>=0; i--){
+            int currBit = (num1 >> i) & 1; // num1 >> i is the current number
+            if((currBit == 1) && count >0){
+                ans = ans | (1<<i);
+                count--;
+            }
+        }
+        // eg num = 1 , num2 = 12
+        for(int i = 0; i<32; i++){
+            if(count ==0)break;
+            int currBit = (ans>>i) & 1;
+            if(currBit !=1){
+                ans = ans | (1<<i);
+                count--;
             }
         }
         
-		// if bits to set are remaining, set the `LSB`
-        for(int i=0;i<32;i++) {
-            if(bit == 0) break;
-			int currBit = (ans >> i) & 1;
-            if(currBit != 1) {
-                ans |= (1 << i);
-                bit--;
-            }
-        }
         return ans;
+        
+        
     }
 }
